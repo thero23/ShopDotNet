57fb6cf268bd08d58e7fa70d2173b184001f0a67
Ильюша :3 <levawinner@gmail.com>
2022-03-23 21:46:00 +0300

    add provide country (#5)

---
+++ InternetShop/InternetShop/ViewModels/ProducerCountry/ProducerCountryModelView.cs
@@ -0,0 +1,8 @@
+﻿namespace InternetShop.ViewModels.ProducerCountry
+{
+    public class ProducerCountryModelView
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
