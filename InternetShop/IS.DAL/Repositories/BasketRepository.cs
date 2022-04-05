8e530880b15259b73769187da3a32a64f0ff4bc9
thero23 <levawinner@gmail.com>
2022-04-06 01:50:18 +0300

    add DAL

---
+++ InternetShop/IS.DAL/Repositories/BasketRepository.cs
@@ -0,0 +1,13 @@
+﻿using IS.DAL.Contexts;
+using IS.DAL.Entities;
+using IS.DAL.Interfaces;
+
+namespace IS.DAL.Repositories
+{
+    public class BasketRepository : GenericRepository<BasketEntity>, IBasketRepository
+    {
+        public BasketRepository(DatabaseContext context) : base(context)
+        {
+        }
+    }
+}
